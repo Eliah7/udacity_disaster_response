252b8c7916a8d09bb290bd21bfef6d36ebbc843d
Eliah7 <eliahmbwilo7@gmail.com>
2024-03-28 14:05:40 +0000

    load data train_classifer.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,87 @@
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(database_filepath):
-    pass
+    """
+    Load data from an SQLite database into a pandas DataFrame.
 
+    Args:
+        database_filepath (str): Filepath of the SQLite database.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing the loaded data.
+    """
+    engine = create_engine(f'sqlite:///{database_filepath}.db')
+    query = "SELECT * FROM categories;"
+    df = pd.read_sql(query, engine)
+    print(df.head())
+    X = df.message.values
+    Y = df.genre.values    
+    category_names = list(df.columns)[3:]
+    print(X, Y, category_names)
+    return X, Y, category_names
 
 def tokenize(text):
+    """
+    Tokenize text data.
+
+    Args:
+        text (str): Text data to be tokenized.
+
+    Returns:
+        list: List of tokens.
+    """
     pass
 
 
 def build_model():
+    """
+    Build and return a machine learning model.
+
+    Returns:
+        Model: Machine learning model.
+    """
     pass
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Evaluate a machine learning model.
+
+    Args:
+        model: Machine learning model to be evaluated.
+        X_test: Test features.
+        Y_test: True labels for the test data.
+        category_names (list): List of category names.
+
+    Returns:
+        None
+    """
     pass
 
 
 def save_model(model, model_filepath):
+    """
+    Save a machine learning model to a file.
+
+    Args:
+        model: Machine learning model to be saved.
+        model_filepath (str): Filepath to save the model.
+
+    Returns:
+        None
+    """
     pass
 
 
 def main():
+    """
+    Main function to train and evaluate a machine learning model.
+
+    Reads command line arguments, loads data, trains the model, evaluates it, and saves the       model.
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))

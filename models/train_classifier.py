e5b325fb7e6dfe1b525cd35c49632a1b0ea7f770
Eliah7 <eliahmbwilo7@gmail.com>
2024-03-29 09:27:10 +0000

    train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -2,6 +2,16 @@ import sys
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selction import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 def load_data(database_filepath):
     """
@@ -33,7 +43,15 @@ def tokenize(text):
     Returns:
         list: List of tokens.
     """
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clearn_tokens.append(clean_tok)
+    
+    return clean_tokens
 
 
 def build_model():
@@ -43,7 +61,24 @@ def build_model():
     Returns:
         Model: Machine learning model.
     """
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            'text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])
+        ])),
+        ('clf', RandomForestClassifier())
+    ])
+    
+    parameters = {
+        'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [2, 3, 4]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -59,7 +94,14 @@ def evaluate_model(model, X_test, Y_test, category_names):
     Returns:
         None
     """
-    pass
+    Y_pred = model.predict(X_test)
+    confusion_mat = confusion_matrix(Y_test, Y_pred, labels=category_names)
+    accuracy = (Y_pred == Y_test).mean()
+    
+    print("CATEGORIES: ", category_names)
+    print("ACCURACY: ", accuracy)
+    print("CONFUSION MATRIX: \n", confusion_mat)
+    print("\nBEST PARAMETERS: ", model.best_params_)
 
 
 def save_model(model, model_filepath):
@@ -73,7 +115,8 @@ def save_model(model, model_filepath):
     Returns:
         None
     """
-    pass
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
